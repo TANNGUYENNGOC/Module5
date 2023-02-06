d463f66a67e58de4a4280a76d43485ba0190d95c
TANNGUYENNGOC <nguyentantai1610@gmail.com>
2023-02-07 01:11:26 +0700

    update

--- re-furama-management/src/app/component/customer/list-customer/list-customer.component.ts
+++ re-furama-management/src/app/component/customer/list-customer/list-customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Customer} from "../../../model/customer/customer";
 
 @Component({
   selector: 'app-list-customer',
@@ -6,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-customer.component.css']
 })
 export class ListCustomerComponent implements OnInit {
+  customerList:Customer [] = [
+    {
+      id:1,
+      customerType:{
+        id:1,
+        name:"vip"
+      },
+      name:"tan123azasd",
+      dateOfBirth:"22-02-2000",
+      gender:true,
+      idCard:"123456789",
+      phoneNumber:"00099944332",
+      email:"tan@gmail.com",
+      address:"Quảng ngãi"
+    },
+    {
+      id:1,
+      customerType:{
+        id:1,
+        name:"vip"
+      },
+      name:"tan123azasd",
+      dateOfBirth:"22-02-2000",
+      gender:false,
+      idCard:"123456789",
+      phoneNumber:"00099944332",
+      email:"tan@gmail.com",
+      address:"Quảng ngãi"
+    }
+  ]
 
   constructor() { }
 
